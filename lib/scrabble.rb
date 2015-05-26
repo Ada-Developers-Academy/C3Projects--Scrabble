a8f29df007d3c00ba3a1866dce4f03e924db7639
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-05-26 16:06:39 -0700

    added test and code for .highest_score_from(list)

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,31 +1,44 @@
-class Scrabble
-  # require your gems and classes here
-  # require_relative 'scrabble/whatevs'
-  
-  SCORES = {
-  	1 	=> "aeioulnrst",
-  	2 	=> "dg",
-  	3 	=> "bcmp",
-  	4 	=> "fhvwy",
-  	5 	=> "k",
-  	8 	=> "jx",
-  	10 	=> "qz"
-  }
+module Scrabble
 
-  def initialize
-  end
+	class Scrabble
+	  # require your gems and classes here
+	  # require_relative 'scrabble/whatevs'
+	  
+	  SCORES = {
+	  	1 	=> "aeioulnrst",
+	  	2 	=> "dg",
+	  	3 	=> "bcmp",
+	  	4 	=> "fhvwy",
+	  	5 	=> "k",
+	  	8 	=> "jx",
+	  	10 	=> "qz"
+	  }
 
-  def self.score(word)
-  	word_score = 0
-  	word = word.downcase
-  	# puts word.split("").class
-  	word.split("").each do |letter|
-		SCORES.each do |points, letter_list|
-			if letter_list.split("").include?(letter)
-				word_score += points
+	  def initialize
+	  end
+
+	  def self.score(word)
+	  	word_score = 0
+	  	word_letters = word.downcase.split("")
+	  	# puts word.split("").class
+	  	word_letters.each do |letter|
+			SCORES.each do |points, letter_list|
+				if letter_list.include?(letter)
+					word_score += points
+				end
 			end
-		end
-  	end
-  	word_score
-  end
+	  	end
+	  	word_score
+	  end
+
+
+	  def self.highest_score_from(list_of_words)
+	  	all_scores = []
+	  	list_of_words.each do |word|
+	  		all_scores << [Scrabble.score(word), word]
+	  	end
+	  	return all_scores.max.last
+	  end
+
+	end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -10,12 +10,25 @@ describe "scoring words in Scrabble" do
 		["potato", (3 + 1 + 1 + 1 + 1 + 1)]
 	].each do |word, score|
 		it "returns #{score} for #{word}" do
-			expect(Scrabble.score(word)).to eq(score)
+			expect(Scrabble::Scrabble.score(word)).to eq(score)
 		end
 	end
 
 	it "is case insensitive for input words" do
-		expect(Scrabble.score("potato")).to eq(Scrabble.score("POTATO"))
+		expect(Scrabble::Scrabble.score("potato")).to eq(Scrabble::Scrabble.score("POTATO"))
 	end
-	
+
+
+	[
+		["apple", ["apple", "a", "ap"]],
+		["zzz", ["cab", "pig", "zzz", "zamboni"]]
+	].each do |word, list|
+		it "returns #{word} from list of: #{list}" do
+			expect(Scrabble::Scrabble.highest_score_from(list)).to eq(word)
+		end
+	end
+
 end
+
+# describe "validate user input" do
+# end
