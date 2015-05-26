ccf6c76fa5f14bbf852b38f0d8d374fb4a6a7745
Loraine <loraine@lorainekv.com>
2015-05-26 15:59:35 -0700

    Got rid of the hash array score table. Thanks TA Andrew

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,12 +1,22 @@
 module Scrabble
   # require your gems and classes here
   # require_relative 'scrabble/whatevs'
+  SCORE_TABLE =   {
+    "A" => 1,
+    "E" => 1,
+    "I" => 1,
+    "O" => 1,
+    "U" => 1,
+    "L" => 1,
+    "N" => 1,
+    "R" => 1
+  }
+
   class Scrabble
 
-
-
     def self.score(word)
-      return "1" if word == "A"
+      return SCORE_TABLE[word]
+
     end
 
     def self.highest_score_from(array_of_words)
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,15 +1,24 @@
 require './lib/scrabble'
 
 describe 'Scoring words' do
+
   {
-  1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-  2 => ["D", "G"],
-  3 => ["B", "C", "M", "P"],
-  4 => ["F", "H", "V", "W", "Y"],
-  5 => ["K"],
-  8 => ["J", "X"],
-  10 => ["Q", "Z"],
-  }.each do |score, letter|
+  "A" => 1,
+  "E" => 1,
+  "I" => 1,
+  "O" => 1,
+  "U" => 1,
+  "L" => 1,
+  "N" => 1,
+  "R" => 1,
+  # 1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  # 2 => ["D", "G"],
+  # 3 => ["B", "C", "M", "P"],
+  # 4 => ["F", "H", "V", "W", "Y"],
+  # 5 => ["K"],
+  # 8 => ["J", "X"],
+  # 10 => ["Q", "Z"],
+}.each do |letter, score|
 
     it "returns #{score} for #{letter}" do
         expect(Scrabble::Scrabble.score(letter)).to eq(score)
