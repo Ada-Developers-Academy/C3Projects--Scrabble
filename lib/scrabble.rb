4732cc09b4af2bce3f4e5e3bfb43d99f8aae42de
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-05-26 16:57:04 -0700

    Adding code via TDD.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,4 +1,32 @@
 module Scrabble
   # require your gems and classes here
   # require_relative 'scrabble/whatevs'
+
+  # constant scoring for scrabble
+ LETTERS = {
+   1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+   2=> ["D", "G"],
+   3=> ["B", "C", "M", "P"],
+   4=> ["F", "H", "V", "W", "Y"],
+   5=> "K",
+   8=> ["J", "X"],
+   10=> ["Q", "Z"]
+ }
+
+  class Scrabble
+
+    def chop(word)
+      input_letters = word.split(//)
+    end
+
+    #   def self.score(word)
+    #     chop
+    #   # LETTERS.find do |point, letters|
+    #   #   if
+    #   # end
+    # end
+  end
+
 end
+
+# Scrabble::Scrabble.new
