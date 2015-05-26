2060de5bd7734daa5f57395cf9b12f2a66a514ba
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-05-26 15:26:25 -0700

    added SCORES constant to store letter values

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,8 +1,27 @@
 module Scrabble
   # require your gems and classes here
+  # require_relative 'scrabble/whatevs'
+  
+  SCORES = {
+  	1 	=> "aeioulnrst",
+  	2 	=> "dg",
+  	3 	=> "bcmp",
+  	4 	=> "fhvwy",
+  	5 	=> "k",
+  	8 	=> "jx",
+  	10 	=> "qz"
+  }
 
   def self.score(word)
-  	1
+
+	SCORES.each do |score, letter_list|
+		if letter_list.split("").include?(word)
+			return score
+		end
+	end
+
   end
-  # require_relative 'scrabble/whatevs'
+
+  # puts SCORES[1]
+
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -3,7 +3,10 @@ require './lib/scrabble.rb'
 describe "scoring words in Scrabble" do
 
 	[
-		["a",	1],
+		["a",	 1],
+		["d",	 2],
+		["z",	10],
+		["ap", (1 + 3)]
 	].each do |word, score|
 		it "returns #{score} for #{word}" do
 			expect(Scrabble.score(word)).to eq(score)
