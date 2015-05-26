3c469a1fa5fb080e0396176004f2f0196eb7c87c
Loraine <loraine@lorainekv.com>
2015-05-26 15:24:29 -0700

    Created a scoring hash in the test.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,4 +1,17 @@
 module Scrabble
   # require your gems and classes here
   # require_relative 'scrabble/whatevs'
+  class Scrabble
+
+      
+
+    def self.score(word)
+      return "1" if word == "A"
+    end
+
+    def self.highest_score_from(array_of_words)
+    end
+
+  end
+
 end
---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,14 @@
+require './lib/scrabble'
+
+describe 'Scoring words' do
+  {
+  1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2 => ["D", "G"],
+  3 => ["B", "C", "M", "P"],
+  4 => ["F", "H", "V", "W", "Y"]
+  5 => ["K"]
+  8 => ["J", "X"]
+  10 => ["Q", "Z"]
+  }
+
+end
