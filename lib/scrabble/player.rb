b8d1f6584c144adf2b844e2ff866583344dd80b0
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-05-27 16:32:46 -0700

    organized player_spec into contexts with before(:each) blocks; added test for #play(word) with invalid input

--- lib/scrabble/player.rb
+++ lib/scrabble/player.rb
@@ -4,12 +4,20 @@ module Scrabble
 		attr_accessor :plays
 		attr_reader :name
 
-		def initialize(player_hash)
-			@name = player_hash[:name]
+		# def initialize(player_hash)
+		# 	@name = player_hash[:name]
+		# 	@plays = []
+		# end
+
+		def initialize(name)
+			@name = name
 			@plays = []
 		end
 
 		def play(word)
+			# guard clause for if word is invalid
+			return "ERROR -- invalid input" unless Scrabble.valid_input?(word)
+			# guard clause for if player has already won
 			return false if won? == true
 			plays << word
 		end
@@ -30,6 +38,10 @@ module Scrabble
 		def highest_scoring_word
 			return false if plays == []
 			Scrabble.highest_score_from(plays)
+		end
+
+		def highest_word_score
+			Scrabble.score(highest_scoring_word)
 		end
 	end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,74 +2,80 @@ require './lib/scrabble/player.rb'
 
 describe "Player behavior" do
 
-	# it "new Player instance has name attribute " do
-	# 	expect(Scrabble::Player.new("Susan").name).to eq("Susan")
-	# end
-
 	let(:player) { Scrabble::Player.new(player_options) }
 	let(:default_options) do
-		{ 	name: "Susan",
-			plays: []
-		}
+		( "Susan" )
 	end
 
 	context "With any player it..." do
-		let(:player_options) { default_options }
-		# series of it "" ... blocks
+		let(:player_options) { default_options }		
 
 		it "knows #name" do
-			expect(player.name).to eq(player_options[:name])
+			expect(player.name).to eq(player_options)
 		end
 
 		it "has #plays" do
-			expect(player.plays).to eq(player_options[:plays])
+			expect(player.plays).to eq([])
 		end
 
-		it "can add plays to #plays" do
-			player.play("pickle")
-			player.play("horse")
-			player.play("donkey")
-			expect(player.plays).to eq(["pickle", "horse", "donkey"])
+		it "returns an error if an invalid word is played" do
+			expect(player.play("appl!e")).to eq("ERROR -- invalid input")
 		end
 
-		it "can return total score" do
-			player.play("pickle")
-			player.play("horse")
-			player.play("donkey")
-			expect(player.total_score).to eq(14+8+14)
+		context "player has played pickle, horse, zydeco" do
+			before(:each) do
+				player.play("pickle")
+				player.play("horse")
+				player.play("zydeco")
+			end
+
+			it "can return highest scoring word" do
+				expect(player.highest_scoring_word).to eq("zydeco")
+			end
+
+			it "it can return highest word score" do
+				expect(player.highest_word_score).to eq(21)
+			end
 		end
 
-		it "can return false if player has already won" do
-			player.play("zydeco")
-			player.play("zamboni")
-			player.play("zydeco")
-			player.play("zydeco")
-			player.play("zydeco")
-			expect(player.play("pickle")).to be false
-		end
+		context "player has not yet won" do
+			before(:each) do
+				player.play("pickle")
+				player.play("horse")
+				player.play("donkey")
+			end
 
-		it "can report win" do
-			player.play("zydeco")
-			player.play("zamboni")
-			player.play("zydeco")
-			player.play("zydeco")
-			player.play("zydeco")
-			expect(player.won?).to be true
-		end
+			it "can add plays to #plays" do
+				expect(player.plays).to eq(["pickle", "horse", "donkey"])
+			end
 
-		it "can return false if player has not won" do
-			player.play("pickle")
-			player.play("horse")
-			player.play("donkey")
-			expect(player.won?).to be false
-		end
+			it "can return total score" do
+				expect(player.total_score).to eq(14+8+14)
+			end
 
-		it "can return highest scoring word" do
-			player.play("pickle")
-			player.play("horse")
-			player.play("zydeco")
-			expect(player.highest_scoring_word).to eq("zydeco")
+			it "can return false if player has not won" do
+				expect(player.won?).to be false
+			end
+		end
+		
+		context "player has won" do
+			before(:each) do
+				player.play("zydeco")
+				player.play("zamboni")
+				player.play("zydeco")
+				player.play("zydeco")
+				player.play("zydeco")
+			end
+
+			it "can return false if player has already won" do
+				expect(player.play("pickle")).to be false
+			end
+
+			it "can report win" do
+				expect(player.won?).to be true
+			end
 		end
 	end
+
 
 end
