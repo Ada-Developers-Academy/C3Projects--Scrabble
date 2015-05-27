d7b4ae512f222320ccfc8c02c9f71783100e9eb5
Victoria <vikshab2010@hotmail.com>
2015-05-27 16:11:09 -0700

    5 tests are green

--- spec/scrable_spec.rb
+++ spec/scrable_spec.rb
@@ -3,20 +3,23 @@ require './lib/scrabble'
 
 describe "class methods" do
    it "returns an error if the user type a number" do
-     expect(Scrabble::Scrabble.score(76)).to eq("Error")
+     expect(Scrabble::Scrabble.score(76)).to eq("Error!")
    end
 
    it "returns an error if the user type a 0" do
-     expect(Scrabble::Scrabble.score(0)).to eq("Error")
+     expect(Scrabble::Scrabble.score(0)).to eq("Error!")
    end
 
   it "returns an error if the user type a space in the word" do
-    expect(Scrabble::Scrabble.score("wo rd")).to eq("Error")
+    expect(Scrabble::Scrabble.score("wo rd")).to eq("Error!")
   end
 
   it "returns an error if the word contains more than 7 letters" do
-    expect(Scrabble::Scrabble.score("Helloworld")).to eq("Error")
+    expect(Scrabble::Scrabble.score("Helloworld")).to eq("Error!")
   end
 
+  it " returns the total score value for the given word" do
+    expect(Scrabble::Scrabble.score("wwqk")).to eq 23
+  end
 
 end
