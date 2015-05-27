0945a04991e82ee9682810e1de7bffb9e9361121
Victoria <vikshab2010@hotmail.com>
2015-05-27 13:30:09 -0700

    First 4 working tests

---
+++ spec/scrable_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require './lib/scrabble'
+
+describe "class methods" do
+   it "returns an error if the user type a number" do
+     expect(Scrabble::Scrabble.score(76)).to eq("Error")
+   end
+
+   it "returns an error if the user type a 0" do
+     expect(Scrabble::Scrabble.score(0)).to eq("Error")
+   end
+
+  it "returns an error if the user type a space in the word" do
+    expect(Scrabble::Scrabble.score("wo rd")).to eq("Error")
+  end
+
+  it "returns an error if the word contains more than 7 letters" do
+    expect(Scrabble::Scrabble.score("Helloworld")).to eq("Error")
+  end
+
+
+end
