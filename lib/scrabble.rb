e100eb8aed959b75afd0e848c2e7a38d344d829b
Elia <elgrenier@gmail.com>
2015-05-27 09:16:54 -0700

    setting up the module with the Scrabble class

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,4 +1,14 @@
+require 'pathname'
+require 'awesome_print'
+
 module Scrabble
   # require your gems and classes here
-  # require_relative 'scrabble/whatevs'
+  require_relative('scrabble_class')
 end
+
+x= Scrabble::Scrabble.score("probably")
+ap x
+
+# score= Scrabble::Scrabble.new
+# chart = score.score_assignment
+# puts chart
