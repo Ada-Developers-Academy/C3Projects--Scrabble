ff4dd1d7b31023745e4ad0558194efe8b14153a0
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-05-26 20:01:26 -0700

    Add self.score functionality, it works. Add more specs.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -14,19 +14,51 @@ module Scrabble
  }
 
   class Scrabble
+    # confirming that input is valid, only accepting letters
+    # def self.is_valid?(word)
+    #   return "error"
+    # end
 
-    def chop(word)
+    def self.score(word)
+      # make everything uppercase to compare to contant scoring
+      # ********* why doesn't word = word.upcase! ***********
+      word.upcase!
+      # calling the validating method
+      # is_valid?(word)
+      # splitting word into an array of letters
       input_letters = word.split(//)
+      total_points = 0
+
+      # for each letter in the input_letters array, iterate over it
+
+      input_letters.each do |letter|
+        LETTERS.each do |points, letters|
+          # for each key/value in LETTERS constant, iterate over this
+          # if the input_letters letter matches the LETTERS letters in the value,
+          # then add the points key to total_points
+          if letters.include?(letter)
+            total_points += points
+          end
+        end
+      end
+
+      return total_points
     end
 
-    #   def self.score(word)
-    #     chop
-    #   # LETTERS.find do |point, letters|
-    #   #   if
-    #   # end
-    # end
+    def self.highest_score_from(array)
+      score_array = []
+      array.each do |word|
+        word_score = Scrabble.score(word)
+        score_array.push(word_score)
+      end
+
+      # make below a loop
+      if score_array[1] > score_array[0]
+        return array[1]
+      end
+
+    end
+
   end
 
 end
-
-# Scrabble::Scrabble.new
