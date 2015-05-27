0c200f6767e3a3cbaa09df671cda634d4ae37166
Anita <asovay@gmail.com>
2015-05-26 21:14:55 -0700

    Cleaned up code for self.score method and renamed constant

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,4 +1,4 @@
-POINT_CONVERSIONS = [
+POINTS = [
   ["A", 1], ["E", 1], ["I", 1], ["O", 1], ["U", 1],
   ["L", 1], ["N", 1], ["R", 1], ["S", 1], ["T", 1],
   ["D", 2], ["G", 2],
@@ -8,35 +8,16 @@ POINT_CONVERSIONS = [
   ["J", 8], ["X", 8],
   ["Q", 10], ["Z", 10]
 ]
-# [ 
-#   [%w(A E I O U L N R S T), 1],
-#   [%w(D G), 2],
-#   [%w(B C M), 3],
-#   [%w(F H V W Y), 4],
-#   [%w(K), 5],
-#   [%w(J X), 8],
-#   [%w(Q Z), 10]
-# ]
 
 module Scrabble
  
   class Score
 
-    attr_reader :score
-
-    def initialize
-    end
-
-    def self.word_to_array(word)
-      @letter_array = word.upcase.split(//)
-    end
-
-
     def self.score(word)
-      word_to_array(word)
+      word = word.upcase.chars
       score = 0
-      POINT_CONVERSIONS.each do |letter, points|
-        @letter_array.each do |word_letter|
+      POINTS.each do |letter, points|
+        word.each do |word_letter|
           if word_letter == letter
             score = score + points
           end
