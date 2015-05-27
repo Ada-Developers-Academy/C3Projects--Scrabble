2ca386be8f252d36ec99d4f7ca4040370bc80a26
Kyna <khn710@gmail.com>
2015-05-27 14:03:46 -0700

    describe class mthods in spec

--- lib/scrabble/scrabble.rb
+++ lib/scrabble/scrabble.rb
@@ -1,18 +1,34 @@
-CONVERSIONS = [
-  [1, ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]],
-  [2, ["D", "G"]],
-  [3, ["B", "C", "M", "P"]],
-  [4, ["F", "H", "V", "W", "Y"]],
-  [5, ["K"]],
-  [8, ["J", "X"]],
-  [10, ["Q", "Z"]]
-]
-
 module Scrabble
 
   class Scrabble
+    CONVERSIONS = [
+      [1, ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]],
+      [2, ["D", "G"]],
+      [3, ["B", "C", "M", "P"]],
+      [4, ["F", "H", "V", "W", "Y"]],
+      [5, ["K"]],
+      [8, ["J", "X"]],
+      [10, ["Q", "Z"]]
+    ]
 
-    def get_conversion_for(letter)
+    def initialize
+      @array_of_words = ["stop", "go", "peanut", "candy", "zoo", "sailing"]
+    end
+
+    def self.score(word)
+      total_points = 0
+      word.split.each do |letter|
+        letter_to_point(letter)
+        total_points += point
+      end
+      return total_points
+    end
+
+    def self.highest_score_from(array_of_words)
+      
+    end
+
+    def letter_to_point(letter)
       CONVERSIONS.find do |point, letter_array|
         if letter_array.include?(letter)
           return point
--- spec/lib/scrabble_spec.rb
+++ spec/lib/scrabble_spec.rb
@@ -1,18 +1,33 @@
 require './lib/scrabble/scrabble.rb'
 
 describe Scrabble::Scrabble do
-  subject {Scrabble::Scrabble.new}
 
- context "converting letters to number scores" do
-    CONVERSIONS.each do |point, letter_array|
-      letter_array.each do |letter|
-      if letter_array.include?(letter)
-        it "returns #{point} for #{letter}" do
-        expect(subject.get_conversion_for(letter)).to eq(point)
-        end
-      end
+  describe "class methods" do
+    it "responds to 'score'" do
+      expect(Scrabble::Scrabble).to respond_to :score
+    end
+
+    it "responds to 'highest_score_from'" do
+      expect(Scrabble::Scrabble). to respond_to :highest_score_from
     end
   end
-end
+
+  describe "associations" do
+#  subject {Scrabble::Scrabble.new}
+#
+# context "converting letters to number scores" do
+#    CONVERSIONS.each do |point, letter_array|
+#      letter_array.find do |letter|
+#        it "returns #{point} for #{letter}" do
+#        expect(subject.letter_to_point(letter)).to eq(point)
+#        end
+#    end
+#  end
+#end
+#
+#    it "totals points for a word" do
+#      expect(subject.word_to_points(word)).to eq(total_points)
+#    end
+  end
   
 end
