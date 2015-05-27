3b43d3809dd12c0a0f0f7db6b91371ed7e3e6896
Anita <asovay@gmail.com>
2015-05-27 16:20:27 -0700

    Passed rspec tests for score and highest score

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -2,7 +2,7 @@ POINTS = [
   ["A", 1], ["E", 1], ["I", 1], ["O", 1], ["U", 1],
   ["L", 1], ["N", 1], ["R", 1], ["S", 1], ["T", 1],
   ["D", 2], ["G", 2],
-  ["B", 3], ["C", 3], ["M", 3],
+  ["B", 3], ["C", 3], ["M", 3], ["P", 3],
   ["F", 4], ["H", 4], ["V", 4], ["W", 4], ["Y", 4],
   ["K", 5],
   ["J", 8], ["X", 8],
@@ -10,29 +10,48 @@ POINTS = [
 ]
 
 module Scrabble
- 
-  class Score
+  class Scrabble
 
+    # Returns the score of a word, also checking for non-letter characters
+    # If the word contains 7 characters 50 bonus points are rewarded
     def self.score(word)
+      alphabet = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
       word = word.upcase.chars
       score = 0
-      POINTS.each do |letter, points|
-        word.each do |word_letter|
-          if word_letter == letter
-            score = score + points
+      (0..word.length).each do |index|
+        if alphabet.include?(word[index])
+          POINTS.each do |letter, point|
+            word.each do |word_letter|
+             if word.length > 7
+              return "Please enter a valid word."
+             elsif word_letter == letter
+              score = score + point
+             end
+            end
           end
+        else
+          return "Please enter a valid word."
         end
+      if word.length == 7
+        score += 50
       end
       return score
-
       puts "You received #{score} points for #{word}."
+     end
     end
 
+    # Returns the word in the array with the highest score
+    # If multiple words have the same score, the first one is returned
+    # Unless the word has 7 characters
     def self.highest_score_from(array_of_words)
-      # returns the word in the array with the highest score
-      # choose word with fewer tiles, unless all 7 tiles are used
-      # if multiple words with same score, choose first one in the list
+      # array_of_words = %w(cat horse farm piglet tractor spider)
+      sorted_array = array_of_words.sort_by do |word|
+        word.length
+      end
+      sorted_array.max_by do |word|
+       self.score(word)
+      end
     end
+
   end
-  # require_relative 'scrabble/whatevs'
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,24 +1,53 @@
 require './lib/scrabble'
 
-describe Scrabble::Score do
+describe Scrabble::Scrabble do
 
   describe "class methods" do
-    it "returns 5 points for the word 'cat'" do
-      expect(Scrabble::Score.score("cat")).to eq(5)
+    context "returns the score for a given word" do
+
+      it "returns 5 points for the word 'cat'" do
+        expect(Scrabble::Scrabble.score("cat")).to eq(5)
+      end
+
+      it "returns 8 points for the word 'horse'" do
+        expect(Scrabble::Scrabble.score("horse")).to eq(8)
+      end
+
+      it "rewards 50 bonus points for 'tractor'" do
+        expect(Scrabble::Scrabble.score("tractor")).to eq(59)
+      end
+
+      it "recognizes invalid user input: numbers" do
+        expect(Scrabble::Scrabble.score("9")).to eq("Please enter a valid word.")
+      end
+
+      it "recognizes invalid user input: special chars" do
+        expect(Scrabble::Scrabble.score(",")).to eq("Please enter a valid word.")
+      end
+
+      it "recognizes invalid user input: empty" do
+        expect(Scrabble::Scrabble.score("")).to eq("Please enter a valid word.")
+      end
+
+      it "recognizes invalid user input: >7 characters" do
+        expect(Scrabble::Scrabble.score("irrigation")).to eq("Please enter a valid word.")
+      end
     end
 
-    it "returns 9 points for the word 'farm'" do
-      expect(Scrabble::Score.score("farm")).to eq(9)
+    context "returns the word in the array with the highest score" do
+
+      it "returns the word (horse) with the highest score" do
+        expect(Scrabble::Scrabble.highest_score_from(["cat", "horse"])).to eq("horse")
+      end
+
+      it "returns the word with fewer tiles (farm) if multiple words have the same score" do
+        expect(Scrabble::Scrabble.highest_score_from(["cat", "horse", "piglet", "farm"])).to eq("farm")
+      end
+
+      it "returns the 7 letter word (tractor) when other words have the same score" do
+        expect(Scrabble::Scrabble.highest_score_from(["cat", "horse", "farm", "piglet", "tractor", "spider"])).to eq("tractor")
+      end
+
     end
-
-    # it "should return an Array with the letters of the word" do
-    #   expect (Scrabble::Score.word_to_array("cat")).to eq(["C", "A", "T"])
-    # end
-
-    # it "returns the score of the word using 7 tiles with 50 pt bonus" do
-    #   expect (Scrabble::Score.highest_score_from(array_of_words).to eq(""))
-    # end
-
-
   end
 end
