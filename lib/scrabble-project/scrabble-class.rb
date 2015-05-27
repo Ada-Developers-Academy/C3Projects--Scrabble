ffa5b7f732c856abd63d27e897755fcce2761a23
Sally Moore <sallyamoore@gmail.com>
2015-05-26 21:49:23 -0700

    MDRY'ed score method

--- lib/scrabble-project/scrabble-class.rb
+++ lib/scrabble-project/scrabble-class.rb
@@ -1,6 +1,7 @@
 module Scrabble
 
   class Scrabble
+    # Set constants with general error message and point values of each letter
     WORD_ERROR = "ERROR: Please enter a word."
     POINTS = {
       1 => ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"],
@@ -12,6 +13,8 @@ module Scrabble
       7 => ["q", "z"]
     }
 
+    # LETTERS = ("a".."z").to_a
+
     # attr_accessor :score, :word_array
     #
     # def initialize
@@ -19,48 +22,26 @@ module Scrabble
     #   @word_array = []
     # end
 
-
-
     def self.score(word)
+      # error if empty string or nil
       return WORD_ERROR if word == "" || word == nil
+
+      # make all letters lower case, then push letters to array
       word = word.downcase
-      # if word == "a" || word == "e"
-      #   @score = 1
-      #   return @score
-      # end
-      score = 0
       word_array = word.split("").push
 
+      # set initial value of score
+      score = 0
+
+      # for each letter in word_array, check POINTS and add to total to create score
       word_array.each do |letter|
-        if POINTS[1].include?(letter)
-          score += 1
+        for num in 1..7
+          if POINTS[num].include?(letter)
+            score += num
+          end
         end
-
-        if POINTS[2].include?(letter)
-          score += 2
-        end
-
-        if POINTS[3].include?(letter)
-          score += 3
-        end
-
-        if POINTS[4].include?(letter)
-          score += 4
-        end
-
-        if POINTS[5].include?(letter)
-          score += 5
-        end
-
-        if POINTS[6].include?(letter)
-          score += 6
-        end
-
-        if POINTS[7].include?(letter)
-          score += 7
-        end
-
       end
+
       return score
 
     end
