f540a711ecd9a249c17c1f1a363ce9690317ea1c
Elia <elgrenier@gmail.com>
2015-05-27 16:22:40 -0700

    working .score method

--- lib/scrabble_class.rb
+++ lib/scrabble_class.rb
@@ -1,5 +1,6 @@
 module Scrabble
   class Scrabble
+    attr_accessor :total_points, :word
     SCORE = {"a" => 1, "e" => 1, "i" => 1, "o" => 1, "u" => 1,
              "l" => 1, "n" => 1, "r" => 1, "s" => 1, "t" => 1,
              "d" => 2, "g" => 2, "b" => 3, "c" => 3, "m" => 3,
@@ -12,39 +13,19 @@ module Scrabble
 
     def self.score(word)
       total_points = 0
-      word_array = word.chars.map { |ch| ch.downcase}
+      raise ArgumentError.new "You can only use strings" unless word.class == String
+
+      word_array = word.chars.map { |each| each.downcase}
+      raise ArgumentError.new "You can only use standard English letters" unless word_array.all? { |letter| SCORE[letter] }
+
       word_array.each do |letter|
         points = SCORE[letter]
-          total_points += points
-          puts points
-        end
-        return total_points 
+        total_points += points
+        puts points
       end
-    #   # word= word.split(//)
-    #   # word = score_assignment(word)
-    #   # letter = point,
-    # end
+      return total_points
+    end
 
-    # def score_assignment
-    #     # SCORE.find_all do |letter, point|
-    #     #   ap letter
-    #     #   ap point
-    #     # end
-    #     puts SCORE[0]
-    #     puts SCORE[1]
-    # end
-          # word.each do |input|
-          #   if input == letter
-          #   return letter = point
-          #   point += total_points
-          #   # puts letter
-          # # else
-          # #   "Please enter a word"
-          #   end
-          #   puts total_points
-          # end
-    #     end
-    # end
 
     def self.highest_score_from(array_of_words)
     end
