4261a64112c7fda5da253040ddcc706329b36035
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-05-28 13:59:38 -0700

    removed old duplicate methods

--- lib/scrabble/scrabble.rb
+++ lib/scrabble/scrabble.rb
@@ -1,18 +1,6 @@
 module Scrabble
 
 	class Scrabble
-	  # require your gems and classes here
-	  # require_relative 'scrabble/whatevs'
-	  	
-		# POINTS = {
-		#   	"aeioulnrst" 	=> 1,
-		#   	"dg"			=> 2,
-		#   	"bcmp"		 	=> 3,
-		#   	"fhvwy" 		=> 4,
-		#   	"k"			 	=> 5,
-		#   	"jx"		 	=> 8,
-		#   	"qz"		 	=> 10
-	 #  	}
 
 	 	POINTS = {
 		  	"a" => 1, "b" => 3, "c" => 3, "d" => 2, "e" => 1, "f" => 4,
@@ -21,8 +9,6 @@ module Scrabble
 		  	"s" => 1, "t" => 1, "u" => 1, "v" => 4, "w" => 4, "x" => 8,
 		  	"y" => 4, "z" => 10
 	  	}
-
-	  	# VALID_LETTERS = POINTS.keys.reduce(:+)
 
 	  	VALID_LETTERS = POINTS.keys.reduce(:+)
 
@@ -49,19 +35,6 @@ module Scrabble
 		  	true
 	  	end
 
-	 #  	def self.get_points(word)
-		#   	word_score = 0
-		#   	word_letters = Scrabble.process_input(word)
-
-		#   	word_letters.each do |letter|
-		# 		letter_list, points = POINTS.find { 
-		# 			|letter_list, points| letter_list.include?(letter)
-		# 		}
-		# 		word_score += points
-		#   	end
-		#   	word_score
-		# end
-
 		def self.get_points(word)
 		  	word_letters = Scrabble.process_input(word)
 
@@ -81,29 +54,7 @@ module Scrabble
 	  		Scrabble.get_points(word)
 	  	end
 
-	  	# def self.get_scores(array_of_words)
-		  # 	all_score_word_pairs = []
-		  # 	array_of_words.each do |word|
-		  # 		# guard clause for invalid word in array
-		  # 		return nil unless Scrabble.valid_input?(word)
-
-		  # 		all_score_word_pairs << [Scrabble.score(word), word]
-		  # 	end
-		  # 	all_score_word_pairs
-	  	# end
-
-	  	# def self.get_scores(array_of_words)
-		  # 	all_score_word_pairs = {}
-		  # 	array_of_words.each do |word|
-		  # 		# guard clause for invalid word in array
-		  # 		return nil unless Scrabble.valid_input?(word)
-
-		  # 		all_score_word_pairs[word] = Scrabble.score(word)
-		  # 	end
-		  # 	all_score_word_pairs
-	  	# end
-
-	  	def self.get_scores(array_of_words)
+	  	def self.get_high_scores(array_of_words)
 	  		score_groups = array_of_words.group_by do |word|
 		  		return nil unless Scrabble.valid_input?(word)	  			
 	  			Scrabble.score(word)
@@ -114,76 +65,23 @@ module Scrabble
 	  		score_groups[top_score].flatten	# top words
 	  	end
 
-	  	# def self.high_scores(all_score_word_pairs)
-	  	# 	top_score = all_score_word_pairs.max[0]
-	  	# 	high_score_pairs = all_score_word_pairs.find_all { 
-	  	# 		|score, word| score == top_score 
-	  	# 	}
-	  	# end
-
-	  	# def self.high_scores(all_score_word_pairs)
-	  	# 	top_pair = all_score_word_pairs.max_by { |score, word| score }
-	  	# 	top_score = top_pair[0]
-	  	# 	high_score_pairs = all_score_word_pairs.find_all { 
-	  	# 		|score, word| score == top_score 
-	  	# 	}
-	  	# end
-
-	  	# def self.high_scores(all_score_word_pairs)
-	  	# 	top_score = all_score_word_pairs.values.max
-	  	# 	high_score_pairs = all_score_word_pairs.find_all { 
-	  	# 		|word, score| score == top_score 
-	  	# 	}
-	  	# end
-
-	  	# def self.seven_letter_word(high_score_pairs)
-	  	# 	seven_letter_word = high_score_pairs.find { 
-		  # 		|word, score| word.length == 7 
-		  # 	}
-		  # 	if seven_letter_word
-		  # 		return seven_letter_word[0]
-		  # 	else
-		  # 		return nil
-		  # 	end
-	  	# end
-
 	  	def self.seven_letter_word(high_score_words)
 	  		seven_letter_word = high_score_words.find do 
 		  		|word, score| word.length == 7 
 		  	end
 	  	end
 
-	  	# def self.highest_word(high_score_pairs)
-	  	# 	winning_word, score = high_score_pairs.min_by { |word, score| word.length }
-		  # 	winning_word
-	  	# end
-
 	  	def self.highest_word(high_score_words)
 	  		high_score_words.min_by { |word| word.length }
 	  	end
 
-	  	# def self.highest_score_from(array_of_words)
-	  	# 	# get all scores
-	  	# 	all_score_word_pairs 	= Scrabble.get_scores(array_of_words)
-	  	# 	# guard clause for invalid word in array
-	  	# 	return "ERROR -- invalid word in input" unless all_score_word_pairs
-	  	# 	high_score_pairs 		= Scrabble.high_scores(all_score_word_pairs)
-
-		  # 	# pick a seven-letter word in case of a tie
-		  # 	top_word 				= Scrabble.seven_letter_word(high_score_pairs)
-	  	# 	return top_word if top_word
-		  # 	# else pick the shortest word
-		  # 	Scrabble.highest_word(high_score_pairs)
-	  	# end
-
 	  	def self.highest_score_from(array_of_words)
-	  		# get all scores
-	  		high_score_words	= Scrabble.get_scores(array_of_words)
+	  		high_score_words	= Scrabble.get_high_scores(array_of_words)
 	  		# guard clause for invalid word in array
 	  		return "ERROR -- invalid word in input" unless high_score_words
 	  		
 		  	# pick a seven-letter word in case of a tie
-		  	top_word 	= Scrabble.seven_letter_word(high_score_words)
+		  	top_word 			= Scrabble.seven_letter_word(high_score_words)
 	  		return top_word if top_word
 		  	# else pick the shortest word
 		  	Scrabble.highest_word(high_score_words)
