543bffec90a289cced8c35b39047751ba697d621
Anita <asovay@gmail.com>
2015-05-27 19:40:33 -0700

    Started Lunch tier

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,24 +2,16 @@ require './lib/player'
 
 describe Scrabble::Player do
 
-  describe "attributes" do
+  describe "Creating a player for Scrabble" do
+    context "Scrabble needs players with names" do
     let(:player) { Scrabble::Player.new("Samanfar") }
-    # 1,People's Co-op Farmers FarMar::Market,30,Portland,Multnomah,Oregon,97202
-
-    it "has the name 'Samanfar'" do
-      expect(player.name).to eq "Samanfar"
+      it "has the name 'Samanfar'" do
+        expect(player.name).to eq "Samanfar"
+      end
     end
 
-  end
-
-  describe "instance methods" do
-    
-    context "returns the @name instance variable" do
-      it "returns "
-    end
-
-    context "returns an Array of the words played by the player" do
-      it "returns the array of words"
+    it "returns ['cat', 'horse', 'piglet', 'farm']" do
+      expect(player)
     end
 
   end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,9 +2,9 @@ require './lib/scrabble'
 
 describe Scrabble::Scrabble do
 
-  describe "class methods" do
-    context "returns the score for a given word" do
+  describe "This method returns the score of a given word" do
 
+    context "Scrabble scores each letter of a word with a specific point value" do
       it "returns 5 points for the word 'cat'" do
         expect(Scrabble::Scrabble.score("cat")).to eq(5)
       end
@@ -12,11 +12,15 @@ describe Scrabble::Scrabble do
       it "returns 8 points for the word 'horse'" do
         expect(Scrabble::Scrabble.score("horse")).to eq(8)
       end
+    end
 
-      it "rewards 50 bonus points for 'tractor'" do
+    context "Players are rewarded 50pts for using all 7 tiles" do
+      it "rewards 50 bonus points for 'tractor' for using 7 tiles" do
         expect(Scrabble::Scrabble.score("tractor")).to eq(59)
       end
+    end
 
+    context "An error message is returned if invalid input is provided" do
       it "recognizes invalid user input: numbers" do
         expect(Scrabble::Scrabble.score("9")).to eq("Please enter a valid word.")
       end
@@ -33,21 +37,29 @@ describe Scrabble::Scrabble do
         expect(Scrabble::Scrabble.score("irrigation")).to eq("Please enter a valid word.")
       end
     end
+  end
 
-    context "returns the word in the array with the highest score" do
-
+  describe "This method returns the word in a provided array with the highest score" do
+    
+    context "In Scrabble, the highest score wins" do
       it "returns the word (horse) with the highest score" do
         expect(Scrabble::Scrabble.highest_score_from(["cat", "horse"])).to eq("horse")
       end
+    end
 
+    context "If multiple words have the same score, the one with fewer tiles is preferred" do
       it "returns the word with fewer tiles (farm) if multiple words have the same score" do
         expect(Scrabble::Scrabble.highest_score_from(["cat", "horse", "piglet", "farm"])).to eq("farm")
       end
+    end
 
+    context "7 letter words receive a bonus in Scrabble" do
       it "returns the 7 letter word (tractor) when other words have the same score" do
         expect(Scrabble::Scrabble.highest_score_from(["cat", "horse", "farm", "piglet", "tractor", "spider"])).to eq("tractor")
       end
+    end
 
+    context "Protection for empty array being passed" do
       it "returns an error message when an empty array is passed" do
         expect(Scrabble::Scrabble.highest_score_from([])).to eq("Error, please provide an array of words.")
       end
