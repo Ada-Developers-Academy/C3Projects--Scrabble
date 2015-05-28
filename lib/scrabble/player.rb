d1b969244f0dc7af1a6edf969645da57fb7312ed
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-05-27 22:29:16 -0700

    Failed to figure out rspec errors with Lunch. Going to remove and clean up before submitting.

--- lib/scrabble/player.rb
+++ lib/scrabble/player.rb
@@ -39,15 +39,5 @@ module Scrabble
     def highest_word_score
       return Scrabble::Scrabble.score(Scrabble::Scrabble.highest_score_from(@plays_array))
     end
-
-# self.new(name): creates a new instance with the instance variable name assigned
-# #name: returns the @name instance variable
-# #plays: returns an Array of the words played by the player
-# #play(word): Adds the input word to the plays Array
-# Returns false if player has already won
-# #total_score: Sums up and returns the score of the players words
-# #won?: If the player has over 100 points, returns true, otherwise returns 'false'
-# #highest_scoring_word: Returns the highest scoring word the user has played.
-# #highest_word_score: Returns the highest_scoring_word score.
   end
 end
