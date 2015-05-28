ca216da0e8adb1888490bc33daa761a3bce1c6b6
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-05-27 22:18:47 -0700

    Adding final commit. Probably.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -21,7 +21,7 @@ module Scrabble
         exit
       end
 
-      possible_letters = %w(q w e r t y u i o p a s d f g h j k l z x c v b n m Q W E R T Y U I O P A S D F G H J K L Z X C V B N M)
+      possible_letters = %w(Q W E R T Y U I O P A S D F G H J K L Z X C V B N M)
 
       input_letters.each do |letter|
 
@@ -40,7 +40,7 @@ module Scrabble
       word.upcase!
       # splitting word into an array of letters
       input_letters = word.split(//)
-      
+
       # calling the validating method is_valid?(word)
       is_valid?(input_letters)
 
