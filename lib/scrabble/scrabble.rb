dc6c106255fb20467e53de4cadea54683a9c699e
Kyna <khn710@gmail.com>
2015-05-27 23:47:02 -0700

    attempted to write block for shortest word

--- lib/scrabble/scrabble.rb
+++ lib/scrabble/scrabble.rb
@@ -26,16 +26,33 @@ module Scrabble
        score = self.score(word_from_array)
        word_value_pairs << [score, word_from_array]
      end
-     score_hash = word_value_pairs.group_by { |pairs| pairs[0] }
-     final_array = score_hash.max_by{|score, pair| score}
-     final_array.flatten!.keep_if{|element| element.class == String}
-     final_array.each do |word|
-       if word.length == 7
-         return word
-       end
-     end
-     return final_array[0]
+     score_to_word_hash = word_value_pairs.group_by { |pairs| pairs[0] }
+     highest_words = score_to_word_hash.max_by{|score, pair| score}
+     highest_words.flatten!.keep_if{|element| element.class == String}
+     # FUNCTIONING 7 LETTER RETURN --------------
+      highest_words.each do |word|
+          if word.length == 7
+            return word
+          end
+        end
+      return highest_words[0]
+
+    # INCORPORATE SHORTEST WORD ------------------
+    #  length_pairs = []
+    #  highest_words.sort_by do |word|
+      #  length = word.length
+      #  length_pairs << [length, word]
+        # end
+      #  if length_pairs[0].include?(7)
+      #
+      #  if array.last.length == 7
+        #  return array.last
+      #  else
+        #  return array.first
+      #  end
    end
+
+
 
     def self.letter_to_point(letter)
       CONVERSIONS.find do |point, letter_array|
