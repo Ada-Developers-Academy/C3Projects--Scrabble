4d01d3ba3f83722d8fedc0733f25628db8414ee7
Elia <elgrenier@gmail.com>
2015-05-27 19:34:52 -0700

    Scrabble class with two class methods and comments

--- lib/scrabble_class.rb
+++ lib/scrabble_class.rb
@@ -1,6 +1,7 @@
 module Scrabble
+
   class Scrabble
-    attr_accessor :total_points, :word
+    # SCORE is constant within the class-- tiles always have the same value
     SCORE = {"a" => 1, "e" => 1, "i" => 1, "o" => 1, "u" => 1,
              "l" => 1, "n" => 1, "r" => 1, "s" => 1, "t" => 1,
              "d" => 2, "g" => 2, "b" => 3, "c" => 3, "m" => 3,
@@ -8,42 +9,54 @@ module Scrabble
              "y" => 4, "k" => 5, "j" => 8, "x" => 8, "q" =>10,
              "z" => 10}
 
-    # def initialize
-    # end
 
     def self.score(word)
       total_points = 0
       # check to see that the input is actually a string (not nil or a number)
       raise ArgumentError.new "You can only use strings" unless word.class == String
 
-      # check that the string only contains valid English letters, no numbers or symbols
+      # check that the string only contains valid English letters, no numbers or symbols, controls for case
       word_array = word.chars.map { |each| each.downcase}
       raise ArgumentError.new "You can only use standard English letters" unless word_array.all? { |letter| SCORE[letter] }
 
-      #assigning a point value
+      # assigning a point value to each letter in the array
       word_array.each do |letter|
         points = SCORE[letter]
+        # adds points for each letter to a total_points variable
         total_points += points
-        # puts points
       end
       return total_points
     end
 
 
     def self.highest_score_from(array_of_words)
-    equal_words = []
+    # transforms array_of_words into words_hash where key == word and value == word score
     words_hash = Hash[array_of_words.map {|word, total_points| [word, self.score(word)]}]
-    max_quantity = words_hash.values.max
-    max_words = words_hash.select {|k, v| v == max_quantity}.keys
+
+    # finds what the highest score is for the provided array of words
+    highest_score = words_hash.values.max
+
+    # iterates through the hash and returns an array of ALL the words that have the highest score
+    # this prevents only the first highest score from being returned (issue with max_by)
+    max_words = words_hash.select {|k, v| v == highest_score}.keys
+
+    # sorts the array by the length of the words
+    max_words = max_words.sort_by(&:length)
+
+    # creates a length array in order to handle the case of a 7-letter word
+    length = []
+    max_words.map {|word| length << word.length }
+
+    # if/else block to return the correct word in cases of matching highest score
       if max_words.length == 1
-        return max_words[0]
+        return max_words[0] # returns the word if there is only one
+
+      elsif length.include? 7
+        return max_words[-1] #returns the last element-- why the array was sorted before
+
       else
-        return max_words
+        return max_words.min_by(&:length) # returns the shortest word if have no 7-letter word
       end
     end
-
-
-
-
   end
 end
