7b277f94c2f0cbc43749809de8fa15472e56c702
Loren Wang <wang131.131@gmail.com>
2015-06-10 20:50:01 -0700

    cleaned it up a bit, fixed my logic

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,64 +1,39 @@
+require 'pry'
 module Scrabble ##require your gems and classes here # require_relative 'scrabble/whatevs'
 
   SCOREBOARD = {
-    "A" => 1,
-    "E" => 1,
-    "I" => 1,
-    "O" => 1,
-    "U" => 1,
-    "L" => 1,
-    "N" => 1,
-    "R" => 1,
-    "S" => 1,
-    "T" => 1,
-    "D" => 2,
-    "G" => 2,
-    "B" => 3,
-    "C" => 3,
-    "M" => 3,
-    "P" => 3,
-    "F" => 4,
-    "H" => 4,
-    "V" => 4,
-    "W" => 4,
-    "Y" => 4,
+    "A" => 1,  "E" => 1, "I" => 1, "O" => 1, "U" => 1,
+    "L" => 1,  "N" => 1, "R" => 1, "S" => 1, "T" => 1,
+    "D" => 2,  "G" => 2,
+    "B" => 3,  "C" => 3, "M" => 3, "P" => 3,
+    "F" => 4,  "H" => 4, "V" => 4, "W" => 4, "Y" => 4,
     "K" => 5,
-    "J" => 8,
-    "X" => 8,
-    "Q" => 10,
-    "Z" => 10,
+    "J" => 8,  "X" => 8,
+    "Q" => 10, "Z" => 10,
   }
 
   class Scrabble
 
-    def self.letters_in_word(word)
-      return word.split(//)
-    end
 
     def self.score(word)
 
-      return "Must pick a letter!" unless word[/[a-zA-Z]+/] == word && word.length > 0
-      #If the input is a character that isn't a letter, or if it's an empty string, it brings up an error
-
-      # empty array to iterate through each letter of the split word
-      score_array = []
-
-      #assign split word array to a variable
-      split_letter_array = letters_in_word(word)
-
-      #for each letter in the array of individual letters, I pushed the matching letters from SCOREBOARD to score_array
-      #SCOREBOARD[letter.upcase] gives me the score value from my SCOREBOARD hash
-      split_letter_array.each do |letter|
-        score_array.push(SCOREBOARD[letter.upcase])
+      if word[/[a-zA-Z]+/] != word || word.length < 1
+      # if the input isn't a valid character OR word has 0 letters ("" or nil), then it raises an error
+        raise ArgumentError.new("Must input a valid letter!")
       end
 
+      word_split = word.chars
+      # setting the array of split words into a variable
+
+      score_array = word_split.map do |letter|
+        SCOREBOARD[letter.upcase]
+      end
 
       sum = 0
-      # for each letter
+
       score_array.each do |score|
         sum += score.to_i #adds all the letter scores together
       end
-      #returns the sum/score of all letters
       return sum
     end
 
@@ -71,31 +46,30 @@ module Scrabble ##require your gems and classes here # require_relative 'scrabbl
 
       # if there is a word that contains things other than letters, it brings up an error.
       array_of_words.each do |word|
-        puts "Use only letters! Try again" unless word[/[a-zA-Z]+/]  == word && word.length > 0
+        if (word[/[a-zA-Z]+/]  != word) || (word.length < 1)
+        puts "Use only letters! Try again"
+        end
       end
 
-      #picks highest scoring words from array_of_words
       highest_score = array_of_words.max_by { |word| score(word) }
       #picks highest scoring words from array_of_words
 
       tied_words = array_of_words.select { |word| score(word) == score(highest_score) }
       #select all words with highest score including ties
-      minimum_words = tied_words.min_by { |word| word.length }
-      # from tied_words array, it finds the shortest word
+
+      shortest_word = tied_words.min_by { |word| word.length }
+      # finds the shortest word from the tied_words array
 
       seven_letters = tied_words.find { |word| word.length == 7}
-      # tried to find the word with 7 letters from tied_words array
+      # finds the word with 7 letters from tied_words array
+      # if there are multiple 7-letter words that are tied, it will pick the first one in the array due to the "find" method
 
-      tied_words.each do |word|
-        if word.length == 7
-          seven_letters
-        elsif word.length < 7
-          minimum_words
-        end
-        return seven_letters || minimum_words
-        # prefers 7 lettered words when tied. if tied words arent 7 letters, then it returns the shortest word
+      if seven_letters
+        return seven_letters
+        # in a tie, returns word with 7 letters or the first tied word with 7 letters
       end
-
+        return shortest_word
+        # this will happen no matter what (no 7 letter words), so I don't need an else statement
     end
   end
 
